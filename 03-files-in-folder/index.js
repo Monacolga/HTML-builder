7187642ef6e4383866700565f24c6bf1c4da4a9d
Olga <113819035+Monacolga@users.noreply.github.com>
2024-01-19 17:43:35 +0100

    feat: add reader for secret folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, async (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  for (const file of files) {
+    if (file.isFile()) {
+      const filePath = path.join(folderPath, file.name);
+      try {
+        const stats = await fs.promises.stat(filePath);
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKilobytes = fileSizeInBytes / 1024;
+        const fileExtension = path.extname(filePath).slice(1);
+        console.log(
+          `${file.name}-${fileExtension}-${fileSizeInKilobytes.toFixed(3)}kb`,
+        );
+      } catch (err) {
+        console.error(`Error reading file: ${filePath}`, err);
+      }
+    }
+  }
+});
